fc3e01d2abdfaec84b842cef055dd74d33a41936
Xtarii <carlalvinelias@gmail.com>
2025-02-22 14:25:56 +0100

    Created a Typography component which works with theme

--- src/main/java/org/android/ui/text/Typograph.kt
+++ src/main/java/org/android/ui/text/Typograph.kt
@@ -1,1 +1,50 @@
 package org.android.ui.text
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import org.android.ui.styles.Style
+import org.android.ui.styles.theme.useTheme
+
+/**
+ * Text Types
+ */
+enum class TextType(val value: TextUnit) {
+    /**
+     * Title Text
+     */
+    TITLE(28.sp),
+    /**
+     * Medium Title Text
+     */
+    MEDIUM_TITLE(24.sp),
+
+    /**
+     * Paragraph Text
+     */
+    PARAGRAPH(14.sp),
+}
+
+
+
+/**
+ * Typography Text Component
+ */
+@Preview
+@Composable
+fun Typography(text: String = "", type: TextType = TextType.PARAGRAPH, color: String = "text", style: Style = Style()) {
+    val theme = useTheme()
+    val textColor = theme.getColor(color)
+
+
+    Box(modifier = Modifier.padding(style.margin)) {
+        Text(text, fontSize = type.value, color = textColor,
+            modifier = Modifier
+        )
+    }
+}
