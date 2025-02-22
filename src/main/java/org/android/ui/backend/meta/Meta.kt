62d72d81534f47f2fd54c9b68dc24cc7d462521d
Xtarii <carlalvinelias@gmail.com>
2025-02-22 14:23:58 +0100

    Metadata starter page

--- src/main/java/org/android/ui/backend/meta/Meta.kt
+++ src/main/java/org/android/ui/backend/meta/Meta.kt
@@ -1,5 +1,8 @@
 package org.android.ui.backend.meta
 
+import androidx.compose.runtime.Composable
+import org.android.ui.backend.pages.StarterPage
+
 /**
  * Layout Metadata
  */
@@ -8,4 +11,9 @@ data class Meta (
      * Weather or not to use fullscreen
      */
     val fullscreen: Boolean = false,
+
+    /**
+     * The Application Starter Page
+     */
+    val startPage: @Composable () -> Unit = { StarterPage() }
 )
