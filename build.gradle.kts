18ea634521b18be69a39cb84bb193dd399f7a675
Xtarii <carlalvinelias@gmail.com>
2025-02-26 20:39:47 +0100

    Added Async Image from Coil

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
