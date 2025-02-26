36fbeeb92ddcbc46eda2bf70140c7ed8f98bb04c
Xtarii <carlalvinelias@gmail.com>
2025-02-26 20:42:24 +0100

    Card Text

--- src/main/java/org/android/ui/layouts/cards/CardText.kt
+++ src/main/java/org/android/ui/layouts/cards/CardText.kt
@@ -1,1 +1,27 @@
 package org.android.ui.layouts.cards
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import org.android.ui.styles.Style
+import org.android.ui.text.Typography
+
+/**
+ * Default Card Text Style
+ */
+val DefaultCardTextStyle: Style = Style(
+    margin = PaddingValues(start = 12.dp, top = 4.dp),
+)
+
+
+
+/**
+ * Card Component Text
+ *
+ * This is a different Component
+ * from the title
+ */
+@Composable
+fun CardText(text: String, style: Style = DefaultCardTextStyle, color: String = "text") {
+    Typography(text, style = style, color = color)
+}
