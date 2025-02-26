d35f468fc937ac48311c0fb1cbe252451c82a704
Xtarii <carlalvinelias@gmail.com>
2025-02-26 20:40:36 +0100

    Activity Provider for better context and window references

--- src/main/java/org/android/ui/backend/router/Activity.kt
+++ src/main/java/org/android/ui/backend/router/Activity.kt
@@ -1,1 +1,44 @@
 package org.android.ui.backend.router
+
+import android.content.Context
+import android.view.Window
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.platform.LocalContext
+import org.android.ui.layouts.RootLayout
+
+/**
+ * Local Window Context
+ */
+private val localWindow = compositionLocalOf<Window> {
+    error("No Activity Provider Present in the Layout")
+}
+
+
+
+/**
+ * Activity Provider
+ */
+@Composable
+fun ActivityProvider(activity: RootLayout, children: @Composable () -> Unit) {
+    CompositionLocalProvider(localWindow provides activity.window) {
+        children()
+    }
+}
+
+
+
+
+
+/**
+ * Window State
+ */
+@Composable
+fun useWindow(): Window = localWindow.current
+
+/**
+ * Context State
+ */
+@Composable
+fun useContext(): Context = LocalContext.current
