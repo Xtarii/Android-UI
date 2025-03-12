3427eac9d62c55a84387b8050212599b495b0f78
Xtarii <carlalvinelias@gmail.com>
2025-03-12 09:54:16 +0100

    static and basepath

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/Android-UI/docs"
 };
 
 export default nextConfig;
